fd4a1f8caa2a5fb14955f4583666f2ecf6222eb1
Lars Christian Jensen <larschri@pvv.ntnu.no>
2020-10-06 21:57:15 +0200

    Put server stuff into server directory

--- main.go
+++ main.go
@@ -1,103 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"github.com/larschri/blaneblikk/dataset"
 	"github.com/larschri/blaneblikk/dataset/dtm10utm32"
-	"github.com/larschri/blaneblikk/render"
-	"image/png"
-	"log"
-	"math"
-	"net"
-	"net/http"
+	"github.com/larschri/blaneblikk/server"
 	_ "net/http/pprof"
 	"path/filepath"
-	"strconv"
 )
-
-var elevmap dataset.ElevationMap
-
-func getFloatParam(req *http.Request, param string) float64 {
-	f, err := strconv.ParseFloat(req.URL.Query().Get(param), 64)
-	if err != nil {
-		panic(err)
-	}
-	return f
-}
-
-func getIntParam(req *http.Request, param string) int {
-	num, err := strconv.ParseInt(req.URL.Query().Get(param), 10, 32)
-	if err != nil {
-		panic(err)
-	}
-	return int(num)
-}
-
-func requestToRenderer(req *http.Request) render.Renderer {
-	width := math.Pi * 2 / 64
-	easting, northing := dtm10utm32.DTM10UTM32Dataset.Translate(getFloatParam(req, "lat0"), getFloatParam(req, "lng0"))
-	easting1, northing1 := dtm10utm32.DTM10UTM32Dataset.Translate(getFloatParam(req, "lat1"), getFloatParam(req, "lng1"))
-	angle := -math.Atan2(easting-easting1, northing1-northing)
-	return render.Renderer{
-		Start:      angle - width/2,
-		Width:      width,
-		Columns:    800,
-		Easting:    easting,
-		Northing:   northing,
-		Elevations: elevmap,
-	}
-}
-
-func writeJSONResponse(w http.ResponseWriter, result interface{}, err error) {
-	if err != nil {
-		w.WriteHeader(400)
-		_, err := w.Write([]byte(err.Error()))
-		if err != nil {
-			log.Printf("failed to write HTTP 400 response: %v", err)
-		}
-		return
-	}
-
-	bytes, err := json.Marshal(result)
-	if err != nil {
-		w.WriteHeader(500)
-		_, err := w.Write([]byte(err.Error()))
-		if err != nil {
-			log.Printf("failed to write HTTP 500 response: %v", err)
-		}
-		return
-	}
-
-	w.Header().Add("Content-Type", "application/json")
-	_, err = w.Write(bytes)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func pixelLatLngHandler(w http.ResponseWriter, req *http.Request) {
-	renderer := requestToRenderer(req)
-	pos, err := renderer.PixelToLatLng(getIntParam(req, "offsetX"), getIntParam(req, "offsetY"))
-	if err != nil {
-		writeJSONResponse(w, nil, err)
-		return
-	}
-	lat, lng := dtm10utm32.DTM10UTM32Dataset.ITranslate(pos.Easting, pos.Northing)
-	writeJSONResponse(w, map[string]interface{}{
-		"lat": lat,
-		"lng": lng,
-	}, nil)
-}
-
-func bbHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type", "image/png")
-	renderer := requestToRenderer(req)
-	err := (&png.Encoder{CompressionLevel: png.BestSpeed}).Encode(w, renderer.CreateImage())
-	if err != nil {
-		log.Printf("failed during image encoding: %v", err)
-	}
-}
 
 func main() {
 	hostPort := flag.String("address", "localhost:8090", "http 'host:port' for the server")
@@ -109,21 +19,10 @@ func main() {
 		panic(err)
 	}
 
-	elevmap, err = dataset.LoadFiles(&dtm10utm32.DTM10UTM32Dataset, *mmapFileDir, files)
+	elevmap, err := dataset.LoadFiles(&dtm10utm32.DTM10UTM32Dataset, *mmapFileDir, files)
 	if err != nil {
 		panic(err)
 	}
 
-	http.HandleFunc("/bb/pixelLatLng", pixelLatLngHandler)
-	http.HandleFunc("/bb", bbHandler)
-	http.Handle("/", http.FileServer(http.Dir("htdocs")))
-
-	listener, err := net.Listen("tcp", *hostPort)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Print("Listening to " + *hostPort)
-
-	_ = http.Serve(listener, nil)
+	server.Serve(elevmap, *hostPort)
 }
---
+++ server/server.go
@@ -0,0 +1,115 @@
+package server
+
+import (
+	"encoding/json"
+	"github.com/larschri/blaneblikk/dataset"
+	"github.com/larschri/blaneblikk/dataset/dtm10utm32"
+	"github.com/larschri/blaneblikk/render"
+	"image/png"
+	"log"
+	"math"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+	"strconv"
+)
+
+var elevmap dataset.ElevationMap
+
+func getFloatParam(req *http.Request, param string) float64 {
+	f, err := strconv.ParseFloat(req.URL.Query().Get(param), 64)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func getIntParam(req *http.Request, param string) int {
+	num, err := strconv.ParseInt(req.URL.Query().Get(param), 10, 32)
+	if err != nil {
+		panic(err)
+	}
+	return int(num)
+}
+
+func requestToRenderer(req *http.Request) render.Renderer {
+	width := math.Pi * 2 / 64
+	easting, northing := dtm10utm32.DTM10UTM32Dataset.Translate(getFloatParam(req, "lat0"), getFloatParam(req, "lng0"))
+	easting1, northing1 := dtm10utm32.DTM10UTM32Dataset.Translate(getFloatParam(req, "lat1"), getFloatParam(req, "lng1"))
+	angle := -math.Atan2(easting-easting1, northing1-northing)
+	return render.Renderer{
+		Start:      angle - width/2,
+		Width:      width,
+		Columns:    800,
+		Easting:    easting,
+		Northing:   northing,
+		Elevations: elevmap,
+	}
+}
+
+func writeJSONResponse(w http.ResponseWriter, result interface{}, err error) {
+	if err != nil {
+		w.WriteHeader(400)
+		_, err := w.Write([]byte(err.Error()))
+		if err != nil {
+			log.Printf("failed to write HTTP 400 response: %v", err)
+		}
+		return
+	}
+
+	bytes, err := json.Marshal(result)
+	if err != nil {
+		w.WriteHeader(500)
+		_, err := w.Write([]byte(err.Error()))
+		if err != nil {
+			log.Printf("failed to write HTTP 500 response: %v", err)
+		}
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	_, err = w.Write(bytes)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func pixelLatLngHandler(w http.ResponseWriter, req *http.Request) {
+	renderer := requestToRenderer(req)
+	pos, err := renderer.PixelToLatLng(getIntParam(req, "offsetX"), getIntParam(req, "offsetY"))
+	if err != nil {
+		writeJSONResponse(w, nil, err)
+		return
+	}
+	lat, lng := dtm10utm32.DTM10UTM32Dataset.ITranslate(pos.Easting, pos.Northing)
+	writeJSONResponse(w, map[string]interface{}{
+		"lat": lat,
+		"lng": lng,
+	}, nil)
+}
+
+func bbHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "image/png")
+	renderer := requestToRenderer(req)
+	err := (&png.Encoder{CompressionLevel: png.BestSpeed}).Encode(w, renderer.CreateImage())
+	if err != nil {
+		log.Printf("failed during image encoding: %v", err)
+	}
+}
+
+func Serve(emap dataset.ElevationMap, hostPort string) {
+	elevmap = emap
+
+	http.HandleFunc("/bb/pixelLatLng", pixelLatLngHandler)
+	http.HandleFunc("/bb", bbHandler)
+	http.Handle("/", http.FileServer(http.Dir("server/static")))
+
+	listener, err := net.Listen("tcp", hostPort)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("Listening to " + hostPort)
+
+	_ = http.Serve(listener, nil)
+}
